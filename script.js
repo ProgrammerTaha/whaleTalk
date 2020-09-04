4b36ef9e9459c448eede1a60f636fa7ee7c505d0
programmertaha <programmer.taha.1y@gmail.com>
2020-09-04 23:18:29 +0300

    iterate through input

--- script.js
+++ script.js
@@ -18,3 +18,8 @@ const vowels = ['a','e','i','o','u'];
 //to store the vowels from the input string
 let resultArray = [];
 
+//loop to iterate through each letter of the input
+for (let i = 0; i < input.length; i++) {
+  ;
+}
+
