465006914a0edafd9c8983f31cc8663b6ea4ed2b
programmertaha <programmer.taha.1y@gmail.com>
2020-09-04 23:21:24 +0300

    nested loops

--- script.js
+++ script.js
@@ -10,16 +10,19 @@ todo: Take a phrase and translate it into its “whale talk” equivalent
 */
 
 //text you want to translate into “whale talk”
-const input= '';
+let input = 'Hello';
+input = input.toUpperCase();
 
 //array of vowels
-const vowels = ['a','e','i','o','u'];
+const vowels = ['A', 'E', 'I', 'O', 'U'];
 
 //to store the vowels from the input string
 let resultArray = [];
 
 //loop to iterate through each letter of the input
 for (let i = 0; i < input.length; i++) {
-  ;
+  for (let j = 0; j < vowels.length; j++) {
+    ;
+  }
 }
 
