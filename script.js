f737b43a19457a3c131542c0d15451379b31b4c1
programmertaha <programmer.taha.1y@gmail.com>
2020-09-04 23:07:26 +0300

    initial comments

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+
+todo: Take a phrase and translate it into its “whale talk” equivalent
+
+% no consonants. Only vowels excluding “y”
+% u‘s and e‘s are doubled
+% result is sung slowly
+
+
+*/
+
