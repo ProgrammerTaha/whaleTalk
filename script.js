9ff4f2406b2587636868fc4ce32c792b5ed13596
programmertaha <programmer.taha.1y@gmail.com>
2020-09-05 00:06:12 +0300

    completing the program

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ todo: Take a phrase and translate it into its “whale talk” equivalent
 */
 
 //text you want to translate into “whale talk”
-let input = 'Hello';
+let input = 'Hi, Human';
 input = input.toUpperCase();
 
 //array of vowels
@@ -19,12 +19,23 @@ const vowels = ['A', 'E', 'I', 'O', 'U'];
 //to store the vowels from the input string
 let resultArray = [];
 
-//loop to iterate through each letter of the input
-for (let i = 0; i < vowels.length; i++) {
-  for (let j = 0; j < input.length; j++) {
-    if (vowels[i] === input[j]) {
-      ;
+/* loop to iterate through each letter of the input 
+then through the vowels then pushing them into 
+a new array */
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j]) {
+      if (input[i] === 'E') {
+        resultArray.push('EE');
+      } else if (input[i] === 'U') {
+        resultArray.push('UU');
+      } else {
+        resultArray.push(input[i]);
+      }
     }
   }
 }
 
+//logging the result
+resultArray = resultArray.join('');
+console.log(resultArray);
